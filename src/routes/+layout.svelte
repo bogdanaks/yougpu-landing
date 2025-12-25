2043396d1bf1a7c05c34241ed4bb93779324335a
Bogdan Aksenov <b.aksenov>
2025-12-25 21:48:39 +0300

    Update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import YandexMetrika from '$widgets/ui/yandex-metrika.svelte';
-	export const prerender = true;
 	import '../app.css';
 	import favicon from '$lib/assets/logo.png';
 
